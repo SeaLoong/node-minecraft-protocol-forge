b1c7c0e58310d8ddcaf1cdd96cbc61bb8374288f
SeaLoong <sealoongw@foxmail.com>
2025-07-22 06:07:20 +0800

    fix: type of ping

--- index.d.ts
+++ index.d.ts
@@ -1,73 +1,92 @@
 /// <reference types="node" />
-import { PingOptions as VanillaPingOptions, OldPingResult, NewPingResult } from 'minecraft-protocol'
+import {
+  PingOptions as VanillaPingOptions,
+  OldPingResult as VanillaOldPingResult,
+  NewPingResult as VanillaNewPingResult,
+} from 'minecraft-protocol';
 
 declare module 'minecraft-protocol-forge' {
 
+  export type OldPingResult = VanillaOldPingResult
+
+  export type NewPingResult = VanillaNewPingResult
+
   export interface PingOptions extends VanillaPingOptions {
-    deserializeForgeData?: boolean
-    overrideForgeData?: boolean
+    deserializeForgeData?: boolean;
+    overrideForgeData?: boolean;
   }
 
   export interface FMLPingResult extends NewPingResult {
     modinfo: {
-      type: string
+      type: string;
       modList: {
-        modid: string
-        version: string
-      }[]
-    }
+        modid: string;
+        version: string;
+      }[];
+    };
   }
 
   export interface FML2PingResult extends NewPingResult {
     forgeData: {
       channels: {
-        res: string
-        version: string
-        required: boolean
-      }[]
+        res: string;
+        version: string;
+        required: boolean;
+      }[];
       mods: {
-        modId: string
-        modmarker: string
-      }[]
-      fmlNetworkVersion: 2
-    }
+        modId: string;
+        modmarker: string;
+      }[];
+      fmlNetworkVersion: 2;
+    };
   }
 
   interface FML3Mod {
-    modId: string
-    modVersion?: string
+    modId: string;
+    modVersion?: string;
   }
 
   interface FML3Channel {
-    channelName: string
-    channelVersion: string
-    requiredOnClient: boolean
+    channelName: string;
+    channelVersion: string;
+    requiredOnClient: boolean;
   }
 
   export interface FML3PingResult extends NewPingResult {
     forgeData: {
-      channels: []
-      mods: []
-      truncated: boolean
-      fmlNetworkVersion: 3
+      channels: [];
+      mods: [];
+      truncated: boolean;
+      fmlNetworkVersion: 3;
       d: {
-        truncated: boolean
+        truncated: boolean;
         mods: (FML3Mod & {
-          channels: FML3Channel[]
-        })[]
-        nonModChannels: FML3Channel[]
-      }
-    }
+          channels: FML3Channel[];
+        })[];
+        nonModChannels: FML3Channel[];
+      };
+    };
   }
 
-  export interface FML3PingResultOverride extends FML3PingResult {
+  export interface FML3PingResultOverride extends NewPingResult {
     forgeData: {
-      channels: FML3Channel[]
-      mods: FML3Mod[]
-    }
+      channels: FML3Channel[];
+      mods: FML3Mod[];
+      truncated: boolean;
+      fmlNetworkVersion: 3;
+    };
   }
 
-  type PingResult = OldPingResult | NewPingResult | FMLPingResult | FML2PingResult | FML3PingResult | FML3PingResultOverride
+  type PingResult =
+    | OldPingResult
+    | NewPingResult
+    | FMLPingResult
+    | FML2PingResult
+    | FML3PingResult
+    | FML3PingResultOverride;
 
-  export function ping(options: PingOptions, callback?: (error: Error, result: PingResult) => void): Promise<PingResult>
+  export function ping(
+    options: PingOptions,
+    callback?: (error: Error, result: PingResult) => void
+  ): Promise<PingResult>;
 }
